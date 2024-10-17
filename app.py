b2b0321dc88f359c3f7eb5642b63d892d290b9a7
johngcarlsson-blai <97477577+johngcarlsson-blai@users.noreply.github.com>
2024-10-17 15:36:56 -0700

    Something something gunicorn?!

--- app.py
+++ app.py
@@ -6,8 +6,4 @@ voila_app = Voila()
 
 # Set Voila configuration options
 voila_app.root_dir = os.getcwd()  # Set root directory
-port = int(os.getenv("PORT", 8000))  # Get the port from the environment
-host = "0.0.0.0"  # Bind to the external interface
-
-# Launch Voila and bind to 0.0.0.0
-voila_app.launch_instance(argv=[f"--port={port}", f"--ip={host}", "--strip_sources=False", "main.ipynb"])
+voila_app.launch_instance(argv=["--no-browser", "--port=8866", "--strip_sources=False", "main.ipynb"])
