49283f9b7cd1e56dfb67a0091db0f344850031e3
johngcarlsson-blai <97477577+johngcarlsson-blai@users.noreply.github.com>
2024-10-17 15:31:57 -0700

    Update app.py again

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import os
-import subprocess
+from voila.app import Voila
 
-notebook_path = "main.ipynb"  # Update this to your notebook path if needed
-port = os.getenv("PORT", 8000)
+# Create Voila instance
+voila_app = Voila()
 
-# Run Voila as a subprocess
-subprocess.run(["voila", notebook_path, "--port", str(port), "--no-browser", "--strip_sources=False"])
+# Set Voila configuration options
+voila_app.root_dir = os.getcwd()  # Set root directory
+voila_app.port = int(os.getenv("PORT", 8000))  # Use the port from environment variable
+
+# Serve the notebook
+voila_app.launch_instance(argv=["--strip_sources=False", "main.ipynb"])
