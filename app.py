5a9264d8b023f3b948e31e449ec107d9924780b4
johngcarlsson-blai <97477577+johngcarlsson-blai@users.noreply.github.com>
2024-10-17 15:34:29 -0700

    Update app.py AGAIN jeez

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ voila_app = Voila()
 
 # Set Voila configuration options
 voila_app.root_dir = os.getcwd()  # Set root directory
-voila_app.port = int(os.getenv("PORT", 8000))  # Use the port from environment variable
+port = int(os.getenv("PORT", 8000))  # Get the port from the environment
+host = "0.0.0.0"  # Bind to the external interface
 
-# Serve the notebook
-voila_app.launch_instance(argv=["--strip_sources=False", "main.ipynb"])
+# Launch Voila and bind to 0.0.0.0
+voila_app.launch_instance(argv=[f"--port={port}", f"--ip={host}", "--strip_sources=False", "main.ipynb"])
